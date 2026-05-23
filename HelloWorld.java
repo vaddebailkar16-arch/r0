a5caabd66f5fbeb498174e1723e2f10aa6dcc52c
vaddebailkar16-arch <vaddebailkar16@gmail.com>
2026-05-23 21:45:26 +0530

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
-class HelloWorld
-  {
+public class HelloWorld{
     public static void main(String[]args)
     {
-      System.out.println("HelloWorld java")
+      System.out.println("HelloWorld java");
     }
   } 
